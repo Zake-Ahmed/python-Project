b5f43fbb8d0844a3d14dcf5f7693887d83f46fdd
Zake-Ahmed <zake3012@gmail.com>
2022-06-27 11:40:03 +0000

    imports updateed

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,5 @@
 from application import app, db
-from application.models import ToDo,Users,Posts
+from application.models import Users,Posts
 from application.forms import TaskForm ,PostForm,UserForm
 from flask import Flask, redirect, url_for, render_template, request
 
