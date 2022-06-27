19a9954d30ef58d1dbae96bb9ea2d3f052656a5b
Zake-Ahmed <zake3012@gmail.com>
2022-06-27 10:39:55 +0000

    index made

--- application/routes.py
+++ application/routes.py
@@ -2,3 +2,9 @@ from application import app, db
 from application.models import ToDo,Users,Posts
 from application.forms import TaskForm ,PostForm,UserForm
 from flask import Flask, redirect, url_for, render_template, request
+
+@app.route('/index')
+def index():
+    posts = Posts.query.all()
+
+    return render_template("task.html", Posts=posts)
