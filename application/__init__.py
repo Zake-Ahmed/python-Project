8d9c7470b801432dc1a413c79e0a83692f05f98a
Zake-Ahmed <zake3012@gmail.com>
2022-06-30 09:33:11 +0000

    update

--- application/__init__.py
+++ application/__init__.py
@@ -5,7 +5,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] =\
-           'sqlite:///' + os.path.join(basedir, 'database.db')
+           'sqlite:///' + os.path.join(basedir, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = "shhhhh"
 
