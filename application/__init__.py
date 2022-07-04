5107fc8df3d7152829455900998851c4bef5c695
Zake-Ahmed <zake3012@gmail.com>
2022-07-04 07:35:33 +0000

    update

--- application/__init__.py
+++ application/__init__.py
@@ -4,7 +4,7 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] =\  
+app.config['SQLALCHEMY_DATABASE_URI'] =\
            'sqlite:///' + os.path.join(basedir, 'data.db') #os.getenv('DATABASE_URI')
            #to connect to the cloud database use this syntax "mysql+pymysql://username:password@host/database_name"
                                                             #Also us the export function so both the secret key and the database links are not pushed up to github
